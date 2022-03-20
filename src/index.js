5aa7d5399f34c2ecf2c4c0f6456ad92dc149a9e0
Professor TKD <iamtapishrawat@gmail.com>
2022-03-20 13:15:48 +0530

    a
    Signed-off-by: Professor TKD <iamtapishrawat@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 
-class App extends React.Component {
+class App extends React.Component { //App banayi jo react ka component ha!
     render(){
+        const name = "Tapish"
         return(
-            <h1>Hello World! my name is Tapish</h1>
+            <h1>Hello {name}</h1>
         );
     }
 }
 
-ReactDOM.render(App, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root")); //render function me jo bhi humne likha vo app me ha aur vo root me display hoga
