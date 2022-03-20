43c365bdfcf1bb267c3666ace71dd67a2cfc7fe1
Professor TKD <iamtapishrawat@gmail.com>
2022-03-20 13:26:11 +0530

    b
    Signed-off-by: Professor TKD <iamtapishrawat@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 
+class AddTasks extends React.Component {
+    render() {
+        return (
+        <div>Add a task</div>
+        )
+    }
+}
+
+class TaskList extends React.Component {
+    render() {
+        return (
+        <div>{this.props.purpose}</div>
+        )
+    }
+}
 class App extends React.Component { //App banayi jo react ka component ha!
     render(){
         const name = "Tapish"
         return(
+            // <h1>Hello {name}</h1>
+            <>
             <h1>Hello {name}</h1>
+                <AddTasks /><TaskList purpose="Tasks to do" /><TaskList purpose="Finished Tasks" /></>
+
         );
     }
 }
